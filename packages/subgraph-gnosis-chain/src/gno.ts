07af10cd931bb2130dc056a7c935ef0d0a34780c
nginnever <ginneversource@gmail.com>
2022-03-08 10:56:28 -0800

    matchstick compiles with transfer event

--- packages/subgraph-gnosis-chain/src/gno.ts
+++ packages/subgraph-gnosis-chain/src/gno.ts
@@ -1,4 +1,5 @@
-import { Address, Bytes, BigInt, store } from "@graphprotocol/graph-ts";
+import { Address, Bytes, BigInt, store, ethereum } from "@graphprotocol/graph-ts";
+import { newMockEvent } from "matchstick-as";
 import { Transfer } from "../generated/ds-gno/GNO";
 import { loadOrCreateUser, ADDRESS_ZERO } from "./helpers";
 
@@ -23,6 +24,47 @@ export function handleTransfer(event: Transfer): void {
     userTo.voteWeight = userTo.voteWeight.plus(event.params.value);
     userTo.save();
   }
+}
+
+export function createTranserEvent(
+  id: i32,
+  from: Address,
+  to: Address,
+  value: BigInt,
+  data: string
+): Transfer {
+  let mockEvent = newMockEvent()
+  let newTransferEvent = new Transfer(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    mockEvent.parameters
+  )
+  newTransferEvent.parameters = new Array()
+  let idParam = new ethereum.EventParam('id', ethereum.Value.fromI32(id))
+  let addressFromParam = new ethereum.EventParam(
+    'fromAddress',
+    ethereum.Value.fromAddress(from)
+  )
+  let addressToParam = new ethereum.EventParam(
+    'toAddress',
+    ethereum.Value.fromAddress(to)
+  )
+  let valueParam = new ethereum.EventParam('value', ethereum.Value.fromUnsignedBigInt(value))
+  let dataParam = new ethereum.EventParam('data', ethereum.Value.fromString(data))
+  // let displayNameParam = new ethereum.EventParam('displayName', ethereum.Value.fromString(displayName))
+  // let imageUrlParam = new ethereum.EventParam('imageUrl', ethereum.Value.fromString(imageUrl))
+
+  newTransferEvent.parameters.push(idParam)
+  newTransferEvent.parameters.push(addressFromParam)
+  newTransferEvent.parameters.push(addressToParam)
+  newTransferEvent.parameters.push(valueParam)
+  newTransferEvent.parameters.push(dataParam)
+
+  return newTransferEvent
 }
 
 export { runTests } from "../tests/gno.test";
--- packages/subgraph-gnosis-chain/tests/gno.test.ts
+++ packages/subgraph-gnosis-chain/tests/gno.test.ts
@@ -1,8 +1,9 @@
 import { clearStore, test, assert } from 'matchstick-as/assembly/index'
+import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
 import { User } from '../generated/schema'
 import { GNO } from '../generated/ds-gno/GNO'
 import { Transfer } from "../generated/ds-gno/GNO";
-import { handleTransfer } from '../src/gno'
+import { createTranserEvent, handleTransfer } from '../src/gno'
 
 export function runTests(): void {
   test('Can call mappings with custom events', () => {
@@ -11,13 +12,16 @@ export function runTests(): void {
     user.save()
 
     // Call mappings
-    // let newGravatarEvent = createNewGravatarEvent(12345, '0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7', 'cap', 'pac')
-
+    let value = BigInt.fromI32(1337)
+    let toAddress = Address.fromString('0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7')
+    let fromAddress = Address.fromString('0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7')
+    let data = '0x01'
+    let newTransferEvent = createTranserEvent(12345, fromAddress, toAddress, value, data)
     // let anotherGravatarEvent = createNewGravatarEvent(3546, '0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7', 'cap', 'pac')
 
-    // handleNewGravatars([newGravatarEvent, anotherGravatarEvent])
+    handleTransfer(newTransferEvent)
 
-    // assert.fieldEquals('Gravatar', 'gravatarId0', 'id', 'gravatarId0')
+    assert.fieldEquals('User', 'gravatarId0', 'id', 'test')
     // assert.fieldEquals('Gravatar', '12345', 'owner', '0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7')
     // assert.fieldEquals('Gravatar', '3546', 'displayName', 'cap')
 
