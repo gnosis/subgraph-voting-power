960334cb3fcf1fd582d513a0b49bd1bee625e2b4
Auryn Macmillan <hi@auryn.xyz>
2022-03-10 17:23:26 -0500

    add entry.save() to loadOrCreateUser

--- packages/subgraph-gnosis-chain/src/helpers.ts
+++ packages/subgraph-gnosis-chain/src/helpers.ts
@@ -87,6 +87,7 @@ export function loadOrCreateUser(address: Address): User {
     entry.mgno = BigInt.fromI32(0);
     entry.lgno = BigInt.fromI32(0);
     entry.deposit = BigInt.fromI32(0);
+    entry.save();
   }
   return entry;
 }
