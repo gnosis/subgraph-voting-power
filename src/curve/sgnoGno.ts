783ca97e112f1124ad240f9fc37b2147b818757f
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-11-03 15:52:21 +0100

    account for SGNO/GNO curve pool + gauge deposits

---
+++ src/curve/sgnoGno.ts
@@ -0,0 +1,53 @@
+import { Transfer } from "../../generated-gc/ds-curve-sgno-gno-gauge-deposit/ERC20";
+import {
+  loadOrCreate as loadOrCreateUser,
+  saveOrRemove as saveOrRemoveUser,
+} from "../helpers/user";
+
+import { ADDRESS_ZERO, ZERO_BI } from "../constants";
+import { Address, log } from "@graphprotocol/graph-ts";
+import { User, WeightedPool } from "../../generated/schema";
+import { handleTransfer as handleTransferForWeightedPool } from "../helpers/weightedPool";
+
+export const SGNO_GNO_POOL_ADDRESS = Address.fromString(
+  "0xBdF4488Dcf7165788D438b62B4C8A333879B7078"
+);
+
+export function handleTransfer(event: Transfer): void {
+  const id = event.address.toHexString();
+  const from = event.params.from;
+  const to = event.params.to;
+  const value = event.params.value;
+
+  // for Curve the we have the following ordering of events:
+  // - reserve0 token transfer event
+  // - reserve1 token transfer event
+  // - LP token transfer event
+  // - AddLiquidity/RemoveLiquidity event
+  // As handleTransferForWeightedPool relies on the pools GNO balance being up-to-date,
+  // we make sure it is, adding the balances of GNO and SGNO
+  const pool = loadOrCreateSgnoGnoPool();
+  if (!pool) throw new Error(`Expected WeightedPool #${id} to exist`);
+  const userEntryForPool = User.load(id);
+  pool.gnoBalance = userEntryForPool
+    ? userEntryForPool.gno.plus(userEntryForPool.sgno)
+    : ZERO_BI;
+  pool.save();
+
+  handleTransferForWeightedPool(event, from, to, value);
+}
+
+function loadOrCreateSgnoGnoPool(): WeightedPool {
+  const id = SGNO_GNO_POOL_ADDRESS.toHexString();
+  let pool = WeightedPool.load(id);
+  if (!pool) {
+    pool = new WeightedPool(id);
+    pool.totalSupply = ZERO_BI;
+    pool.gnoBalance = ZERO_BI;
+    pool.save();
+    log.info("instantiated WeightedPool instance for SGNO/GNO Curve pool: {}", [
+      id,
+    ]);
+  }
+  return pool;
+}
---
+++ src/curve/sgnoGnoGaugeDeposit.ts
@@ -0,0 +1,45 @@
+import { Transfer } from "../../generated-gc/ds-curve-sgno-gno-gauge-deposit/ERC20";
+import {
+  loadOrCreate as loadOrCreateUser,
+  saveOrRemove as saveOrRemoveUser,
+} from "../helpers/user";
+
+import { ADDRESS_ZERO } from "../constants";
+import { Address, log } from "@graphprotocol/graph-ts";
+import { WeightedPool } from "../../generated/schema";
+
+const SGNO_GNO_POOL_ADDRESS = Address.fromString("");
+
+export function handleTransfer(event: Transfer): void {
+  const to = event.params.to;
+  const from = event.params.from;
+
+  // gauge deposit tokens are issues in a ratio of 1:1 to the LP tokens
+  const pool = loadSgnoGnoPool();
+  const transferredGnoValue = pool.gnoBalance
+    .times(event.params.value)
+    .div(pool.totalSupply);
+
+  if (from.toHexString() != ADDRESS_ZERO.toHexString()) {
+    const userFrom = loadOrCreateUser(from);
+    userFrom.stakedGnoSgno = userFrom.stakedGnoSgno.minus(transferredGnoValue);
+    userFrom.voteWeight = userFrom.voteWeight.minus(transferredGnoValue);
+    saveOrRemoveUser(userFrom);
+  }
+
+  if (to.toHexString() != ADDRESS_ZERO.toHexString()) {
+    const userTo = loadOrCreateUser(to);
+    userTo.stakedGnoSgno = userTo.stakedGnoSgno.plus(transferredGnoValue);
+    userTo.voteWeight = userTo.voteWeight.plus(transferredGnoValue);
+    userTo.save();
+  }
+}
+
+function loadSgnoGnoPool(): WeightedPool {
+  const id = SGNO_GNO_POOL_ADDRESS.toHexString();
+  let pool = WeightedPool.load(id);
+  if (!pool) {
+    throw new Error("No WeightedPool instance found for SGNO/GNO Curve pool");
+  }
+  return pool;
+}
