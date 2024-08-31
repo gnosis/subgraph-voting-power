b5c9a46ef02340eb9a6707401f3ba6c4aa1f0f8e
Wagalidoom <lukas-8@live.fr>
2024-08-31 07:24:35 +0200

    fix: accounting deposited gno to withdraw credentials address

--- src/deposit.ts
+++ src/deposit.ts
@@ -1,11 +1,14 @@
 import { DepositEvent } from "../generated-gc/ds-deposit/SBCDepositContract";
 import { loadOrCreate as loadOrCreateUser } from "./helpers/user";
 import { ONE_GNO } from "./constants";
+import { Address, Bytes } from "@graphprotocol/graph-ts";
 
 export function handleDeposit(event: DepositEvent): void {
-  const user = event.transaction.from;
+  const withdrawalCredentials = event.params.withdrawal_credentials;
+  const addressBytes = withdrawalCredentials.slice(12) as Bytes;
+  const userAddress = Address.fromBytes(addressBytes);
 
-  const entry = loadOrCreateUser(user);
+  const entry = loadOrCreateUser(userAddress);
   entry.deposit = entry.deposit.plus(ONE_GNO);
   entry.voteWeight = entry.voteWeight.plus(ONE_GNO);
   entry.save();
