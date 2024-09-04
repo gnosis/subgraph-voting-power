37dce181aa7a5ce93032a8fa374855217d2817a4
Wagalidoom <lukas-8@live.fr>
2024-09-04 10:36:39 +0200

    feat: add state update deposit

--- src/osgnoVault.ts
+++ src/osgnoVault.ts
@@ -7,13 +7,13 @@ import {
 export function handleBurn(event: Burn): void {
     const owner = loadOrCreateUser(event.params.owner);
     owner.osgno = owner.osgno.minus(event.params.shares);
-    owner.voteWeight = owner.voteWeight.minus(event.params.assets);
+    // owner.voteWeight = owner.voteWeight.minus(event.params.assets);
     saveOrRemoveUser(owner);
 }
 
 export function handleMint(event: Mint): void {
     const receiver = loadOrCreateUser(event.params.receiver);
     receiver.osgno = receiver.osgno.plus(event.params.shares);
-    receiver.voteWeight = receiver.voteWeight.plus(event.params.assets);
+    // receiver.voteWeight = receiver.voteWeight.plus(event.params.assets);
     saveOrRemoveUser(receiver);
 }
