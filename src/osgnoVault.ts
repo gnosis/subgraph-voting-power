e62768e6ad47d20e9da50e91ac176407feb39e67
Wagalidoom <lukas-8@live.fr>
2024-09-05 05:04:03 +0200

    feat: add vaultState entity

--- src/osgnoVault.ts
+++ src/osgnoVault.ts
@@ -1,4 +1,6 @@
-import { Burn, Mint } from "../generated-gc/ds-osgno-vault/OsTokenVaultController";
+import { BigInt } from "@graphprotocol/graph-ts";
+import { Burn, Mint, StateUpdated } from "../generated-gc/ds-osgno-vault/OsTokenVaultController";
+import { VaultState } from "../generated/schema";
 import {
     loadOrCreate as loadOrCreateUser,
     saveOrRemove as saveOrRemoveUser,
@@ -16,4 +18,20 @@ export function handleMint(event: Mint): void {
     receiver.osgno = receiver.osgno.plus(event.params.shares);
     // receiver.voteWeight = receiver.voteWeight.plus(event.params.assets);
     saveOrRemoveUser(receiver);
+}
+
+export function handleStateUpdated(event: StateUpdated): void {
+    let vaultState = VaultState.load("VAULT_STATE");
+    if (vaultState == null) {
+        vaultState = new VaultState("VAULT_STATE");
+        vaultState.profitAccrued = BigInt.fromI32(0);
+        vaultState.treasuryShare = BigInt.fromI32(0);
+        vaultState.treasuryAsset = BigInt.fromI32(0);
+        vaultState.rate = BigInt.fromI32(0);
+        vaultState.save();
+    }
+    vaultState.profitAccrued = event.params.profitAccrued;
+    vaultState.treasuryShare = event.params.treasuryShares;
+    vaultState.treasuryAsset = event.params.treasuryAssets;
+    vaultState.save();
 }
