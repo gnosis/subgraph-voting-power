ac061838b1f3999370b9512dd8f1dd21573c47b4
Wagalidoom <lukas-8@live.fr>
2024-10-24 16:37:41 +0200

    feat: tracking deposit GNO

---
+++ src/deposit.ts
@@ -0,0 +1,20 @@
+import { DepositEvent } from "../generated-gc/ds-deposit/SBCDepositContract";
+import { loadOrCreate as loadOrCreateUser } from "./helpers/user";
+import { ONE_GNO } from "./constants";
+import { Address, Bytes } from "@graphprotocol/graph-ts";
+
+export function handleDeposit(event: DepositEvent): void {
+  const withdrawalCredentials = event.params.withdrawal_credentials;
+
+  if (withdrawalCredentials.length != 32) {
+    return;
+  }
+
+  const addressBytes = withdrawalCredentials.subarray(12, 32);
+  const userAddress = Address.fromBytes(Bytes.fromUint8Array(addressBytes));
+
+  const entry = loadOrCreateUser(userAddress);
+  entry.deposit = entry.deposit.plus(ONE_GNO);
+  entry.voteWeight = entry.voteWeight.plus(ONE_GNO);
+  entry.save();
+}
